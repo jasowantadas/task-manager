573fdc8fcd90157efd9163d0d2f255dba46a5fa0
jasowantadas <jasowanta21@gmail.com>
2023-10-27 19:47:57 +0530

    testing out db

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,10 +1,16 @@
 //This file represents the functions of a route
+const Task = require("../models/Task");
 const getAllTasks = (req, res) => {
   res.send("all Itemms");
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(req.body);
+  } catch (err) {
+    console.log(err);
+  }
 };
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
